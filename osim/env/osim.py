a01eaeb9becdeae591cca51c5cebeb68bc28f4bf
whikwon <whikwon@gmail.com>
2018-08-16 12:04:42 +0900

    Add discretization

--- osim/env/osim.py
+++ osim/env/osim.py
@@ -282,6 +282,7 @@ class OsimEnv(gym.Env):
     time_limit = 1e10
 
     prev_state_desc = None
+    bins = np.linspace(0, 1, 11)
 
     model_path = None # os.path.join(os.path.dirname(__file__), '../models/MODEL_NAME.osim')
 
@@ -344,6 +345,8 @@ class OsimEnv(gym.Env):
 
     def step(self, action, project = True):
         action = np.clip(action, 0, 1)
+        action = np.digitize(action, self.osim_model.bins, right=True) / 10
+
         self.prev_state_desc = self.get_state_desc()
         self.osim_model.actuate(action)
         self.osim_model.integrate()
